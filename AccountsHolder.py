78a6e38af299182488484b21df33145028db48d6
Md. Afjal Hosen <96134983+AhMeraj@users.noreply.github.com>
2021-12-22 19:06:35 +0600

    Add files via upload

---
+++ AccountsHolder.py
@@ -0,0 +1,64 @@
+class Database_createion():
+    def Information(self):
+        self.id=id
+        print("Create Account for ",self.id)
+        self.name=str(input(f"Enter the name:"))
+        self.age=int(input(f"Enter the age:"))
+        self.address=str(input(f"Address:"))
+        self.phn=str(input(f"Phone Number:"))
+        self.balance=float(input("Enter your starting balance:"))
+    def Withdraw(self):
+        w_balance=float(input("Amount:"))
+        if(self.balance<w_balance):
+            print("Sorry!You do not have sufficiant balance.")
+            self.w1_balance = float(input(f"Input again\nYour current balance is:{self.balance}\n-"))
+            assert self.w1_balance >= self.balance, "Your process is terminated."
+        else:
+            self.balance = self.balance - w_balance
+    def addMoney(self):
+        a_balance = float(input("Amount:"))
+        self.balance=self.balance+a_balance
+    def Display(self):
+        print("Id=",self.id)
+        print("Name=",self.name)
+        print("Age=",self.age)
+        print("Address=",self.address)
+        print("Phone number:",self.phn)
+        print("Current Balance:",self.balance)
+Data=list()
+id=1250
+n=int(input("Enter the number of Accounts Holder:"))
+File=open("Database.txt","a")
+for i in range(0,n):
+    id=id+1
+    Data.append(0)
+    Data[i]=Database_createion()
+    Data[i].Information()
+    print("Your Account is created.Press Enter to continue:")
+    str(input())
+while(True):
+    choice=int(input("Do you want more operation:\n1.Yes\t2.No\n-"))
+    if(choice==1):
+        id_n=int(input("Enter your id:"))
+        choice=int(input("Which operation you want to perform:\n1.Add Money\t2.Withdraw\t3.Display\n-"))
+        id_n=id_n-1251
+        if(choice==1):
+            Data[id_n].addMoney()
+        elif(choice==2):
+            Data[id_n].Withdraw();
+        elif(choice==3):
+            Data[id_n].Display()
+        else:
+            print("Invalid Choice.")
+    else:
+        break;
+print("Processing\n")
+for i in range(0,n):
+    File.write(f"Name:{Data[i].name}\n")
+    File.write(f"ID:{Data[i].id}\n")
+    File.write(f"Age:{Data[i].age}\n")
+    File.write(f"Address:{Data[i].address}\n")
+    File.write(f"Phone number:{Data[i].phn}\n")
+    File.write(f"Balance:{Data[i].balance}")
+print("Processing is finished.\n")
+File.close()
